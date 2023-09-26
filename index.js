5e2d546ee3c176173833f83428f402b75ec24bcb
Masum Billah <fabrika786@gmail.com>
2023-09-26 12:29:33 +0600

    env edit

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(express.json());
 const server = http.createServer(app);
 
   const { MongoClient, ServerApiVersion } = require('mongodb');
-  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obrngag.mongodb.net/?retryWrites=true&w=majority`;
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}mychat@cluster0.obrngag.mongodb.net/?retryWrites=true&w=majority`;
   
   // Create a MongoClient with a MongoClientOptions object to set the Stable API version
   const client = new MongoClient(uri, {
